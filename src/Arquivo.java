75b1b4dadd0ca0d6f57966bf9963169ebed33e33
Otávio Pace <otavio.pace@pagar.me>
2019-01-08 16:24:39 -0200

    even more bad code

---
+++ src/Arquivo.java
@@ -0,0 +1,11 @@
+Connection conn = null;
+try {
+  conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+        "user=steve&password=blue"); // Noncompliant
+  String uname = "steve";
+  String password = "blue";
+  conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+        "user=" + uname + "&password=" + password); // Noncompliant
+
+  java.net.PasswordAuthentication pa = new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Noncompliant
+
